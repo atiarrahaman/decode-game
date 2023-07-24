36c1a9df1968cbaf5a544466b4bf31c38eca7a25
Atiar rahaman <atiar@Os-MacBook-Air.local>
2023-07-24 10:23:12 +0300

    done

---
+++ decode.py
@@ -0,0 +1,293 @@
+
+print("================ Welcome TO Here Bro ==============")
+
+info=input("We are going to Now Solve Some Encode Problem ,are You Ready? Yes/no:")
+
+if info == 'yes' or 'YES' or'Yes':
+    print("1.Make a Decode It Challange No-1")
+    code_1=input("d3739ae02a3889d7a460087c1a95fef7  input Your Ans:")
+    if code_1=="you need learn more":
+        print("wow,You are Right \n Your Result:",code_1)
+        print("2.Make a Decode It Challange No-2")
+        code_2=input("Lbhe Qernz Arne,pneel Ba   input Your Ans:")
+        if code_2=='Your Dream Near,carry On':
+            print("You Are Right",code_2)
+
+            print("3.Make a Decode It Challange No-3")
+            print("Hint: key is  'A' ascci value")
+            code_3=input('Svanyyl, Lbh ner Ernpu Lbhe Qernz    input Your Ans:')
+            if code_3=='Finally, You are Reach Your Dream':
+                print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+            
+            else:
+               print("Uff,Sorry,Try again")
+               print("3.Make a Decode It Challange No-3")
+               print("Hint: key is  'A' ascci value")
+               code_3=input("Svanyyl, Lbh ner Ernpu Lbhe Qernz    input Your Ans:")
+               if code_3=="Finally, You are Reach Your Dream":
+                 print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+               
+               else:
+                print("Uff,Sorry,Try again")
+                print("3.Make a Decode It Challange No-3")
+                print("Hint: key is  'A' ascci value")
+                code_3=input("Svanyyl, Lbh ner Ernpu Lbhe Qernz   input Your Ans:")
+                if code_3=="Finally, You are Reach Your Dream":
+                    print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+                else:
+                    print("Game over Start again")
+
+        else:
+            print("Uff,Sorry,Try again")
+            code_2=input("Lbhe Qernz Arne,pneel Ba  input Your Ans:")
+            if code_2=="Your Dream Near,carry On":
+               print("wow,You are Right \n Your Result:",code_2)
+               print("3.Make a Decode It Challange No-3")
+               print("Hint: key is  'A' ascci value")
+               code_3=input("Svanyyl, Lbh ner Ernpu Lbhe Qernz    input Your Ans:")
+               if code_3=="Finally, You are Reach Your Dream":
+                 print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+               
+               else:
+                print("Uff,Sorry,Try again")
+                print("3.Make a Decode It Challange No-3")
+                print("Hint: key is  'A' ascci value")
+                code_3=input("Svanyyl, Lbh ner Ernpu Lbhe Qernz   input Your Ans:")
+                if code_3=="Finally, You are Reach Your Dream":
+                    print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+                
+            else:
+                print("Uff,Sorry,Try again")
+                code_2=input("Lbhe Qernz Arne,pneel Ba  input Your Ans:")
+                if code_2=="Your Dream Near,carry On":
+                  print("wow,You are Right \n Your Result:",code_2)
+                  print("3.Make a Decode It Challange No-3")
+                  print("Hint: key is  'A' ascci value")
+                  code_3=input("Svanyyl, Lbh ner Ernpu Lbhe Qernz    input Your Ans:")
+                  if code_3=="Finally, You are Reach Your Dream":
+                     print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+               
+                  else:
+                    print("Uff,Sorry,Try again")
+                    print("3.Make a Decode It Challange No-3")
+                    print("Hint: key is  'A' ascci value")
+                    code_3=input("Svanyyl, Lbh ner Ernpu Lbhe Qernz   input Your Ans:")
+                    if code_3=="Finally, You are Reach Your Dream":
+                      print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+                else:
+                    print("Game Over Start again")
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+    else:
+       print("Uff,Sorry,Try again")
+       code_1=input("d3739ae02a3889d7a460087c1a95fef7   input Your Ans:")
+       if code_1=="you need learn more":
+        print("wow,You are Right \n Your Result:",code_1)
+        print("2.Make a Decode It Challange No-2")
+        code_2=input("Lbhe Qernz Arne,pneel Ba   input Your Ans:")
+        if code_2=='Your Dream Near,carry On':
+            print("You Are Right",code_2)
+
+            print("3.Make a Decode It Challange No-3")
+            print("Hint: key is  'A' ascci value")
+            code_3=input('Svanyyl, Lbh ner Ernpu Lbhe Qernz    input Your Ans:')
+            if code_3=='Finally, You are Reach Your Dream':
+                print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+            
+            else:
+               print("Uff,Sorry,Try again")
+               print("3.Make a Decode It Challange No-3")
+               print("Hint: key is  'A' ascci value")
+               code_3=input("Svanyyl, Lbh ner Ernpu Lbhe Qernz    input Your Ans:")
+               if code_2=="Finally, You are Reach Your Dream":
+                 print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+               
+               else:
+                print("Uff,Sorry,Try again")
+                print("3.Make a Decode It Challange No-3")
+                print("Hint: key is  'A' ascci value")
+                code_3=input("Svanyyl, Lbh ner Ernpu Lbhe Qernz   input Your Ans:")
+                if code_3=="Finally, You are Reach Your Dream":
+                    print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+                else:
+                    print("Game over Start again")
+
+        else:
+            print("Uff,Sorry,Try again")
+            code_2=input("Lbhe Qernz Arne,pneel Ba  input Your Ans:")
+            if code_2=="Your Dream Near,carry On":
+               print("wow,You are Right \n Your Result:",code_2)
+               print("3.Make a Decode It Challange No-3")
+               print("Hint: key is  'A' ascci value")
+               code_3=input('Svanyyl, Lbh ner Ernpu Lbhe Qernz    input Your Ans:')
+               if code_3=='Finally, You are Reach Your Dream':
+                 print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+            
+               else:
+                 print("Uff,Sorry,Try again")
+                 print("3.Make a Decode It Challange No-3")
+                 print("Hint: key is  'A' ascci value")
+                 code_3=input("Svanyyl, Lbh ner Ernpu Lbhe Qernz    input Your Ans:")
+                 if code_2=="Finally, You are Reach Your Dream":
+                   print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+               
+                 else:
+                  print("Uff,Sorry,Try again")
+                  print("3.Make a Decode It Challange No-3")
+                  print("Hint: key is  'A' ascci value")
+                  code_3=input("Svanyyl, Lbh ner Ernpu Lbhe Qernz   input Your Ans:")
+                  if code_3=="Finally, You are Reach Your Dream":
+                    print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+                  else:
+                    print("Game over Start again")
+            else:
+                print("Uff,Sorry,Try again")
+                code_2=input("Lbhe Qernz Arne,pneel Ba   input Your Ans:")
+                if code_2=="Your Dream Near,carry On":
+                    print("wow,You are Right \n Your Result:",code_2)
+                    print("3.Make a Decode It Challange No-3")
+                    print("Hint: key is  'A' ascci value")
+                    code_3=input('Svanyyl, Lbh ner Ernpu Lbhe Qernz    input Your Ans:')
+                    if code_3=='Finally, You are Reach Your Dream':
+                     print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+            
+                    else:
+                      print("Uff,Sorry,Try again")
+                      print("3.Make a Decode It Challange No-3")
+                      print("Hint: key is  'A' ascci value")
+                      code_3=input("Svanyyl, Lbh ner Ernpu Lbhe Qernz    input Your Ans:")
+                      if code_2=="Finally, You are Reach Your Dream":
+                         print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+               
+                      else:
+                         print("Uff,Sorry,Try again")
+                         print("3.Make a Decode It Challange No-3")
+                         print("Hint: key is  'A' ascci value")
+                         code_3=input("Svanyyl, Lbh ner Ernpu Lbhe Qernz   input Your Ans:")
+                         if code_3=="Finally, You are Reach Your Dream":
+                           print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+                         else:
+                            print("Game over Start again")
+                else:
+                    print("Game over Start again")
+          
+
+
+
+
+
+       else:
+           print("Uff,Sorry,Try again")
+           code_1=input("d3739ae02a3889d7a460087c1a95fef7   input Your Ans:")
+           
+           if code_1=="you need learn more":
+             print("wow,You are Right \n Your Result:",code_1)
+             print("2.Make a Decode It Challange No-2")
+             code_2=input("Lbhe Qernz Arne,pneel Ba   input Your Ans:")
+             if code_2=='Your Dream Near,carry On':
+                print("You Are Right",code_2)
+
+                print("3.Make a Decode It Challange No-3")
+                print("Hint: key is  'A' ascci value")
+                code_3=input('Svanyyl, Lbh ner Ernpu Lbhe Qernz    input Your Ans:')
+                if code_3=='Finally, You are Reach Your Dream':
+                   print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+            
+                else:
+                   print("Uff,Sorry,Try again")
+                   code_3=input("Svanyyl, Lbh ner Ernpu Lbhe Qernz    input Your Ans:")
+                   if code_2=="Finally, You are Reach Your Dream":
+                      print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+               
+                   else:
+                      print("Uff,Sorry,Try again")
+                      code_3=input("Svanyyl, Lbh ner Ernpu Lbhe Qernz   input Your Ans:")
+                      if code_3=="Finally, You are Reach Your Dream":
+                         print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+                      else:
+                         print("Game over Start again")
+
+             else:
+               print("Uff,Sorry,Try again")
+               code_2=input("Lbhe Qernz Arne,pneel Ba  input Your Ans:")
+               if code_2=="Your Dream Near,carry On":
+                  print("wow,You are Right \n Your Result:",code_2)
+
+                  #here3
+                  print("3.Make a Decode It Challange No-3")
+                  print("Hint: key is  'A' ascci value")
+                  code_3=input('Svanyyl, Lbh ner Ernpu Lbhe Qernz    input Your Ans:')
+                  if code_3=='Finally, You are Reach Your Dream':
+                    print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+            
+                  else:
+                    print("Uff,Sorry,Try again")
+                    code_3=input("Svanyyl, Lbh ner Ernpu Lbhe Qernz    input Your Ans:")
+                    if code_2=="Finally, You are Reach Your Dream":
+                      print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+               
+                    else:
+                       print("Uff,Sorry,Try again")
+                       code_3=input("Svanyyl, Lbh ner Ernpu Lbhe Qernz   input Your Ans:")
+                       if code_3=="Finally, You are Reach Your Dream":
+                         print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+                       else:
+                         print("Game over Start again")
+                  #THIS ELSE anothe
+               else:
+                 print("Uff,Sorry,Try again")
+                 code_2=input("Lbhe Qernz Arne,pneel Ba   input Your Ans:")
+                 if code_2=="Your Dream Near,carry On":
+                    print("wow,You are Right \n Your Result:",code_2)
+                    #here3
+                    print("3.Make a Decode It Challange No-3")
+                    print("Hint: key is  'A' ascci value")
+                    code_3=input('Svanyyl, Lbh ner Ernpu Lbhe Qernz    input Your Ans:')
+                    if code_3=='Finally, You are Reach Your Dream':
+                      print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+            
+                    else:
+                       print("Uff,Sorry,Try again")
+                       code_3=input("Svanyyl, Lbh ner Ernpu Lbhe Qernz    input Your Ans:")
+                       if code_2=="Finally, You are Reach Your Dream":
+                         print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+               
+                       else:
+                          print("Uff,Sorry,Try again")
+                          code_3=input("Svanyyl, Lbh ner Ernpu Lbhe Qernz   input Your Ans:")
+                          if code_3=="Finally, You are Reach Your Dream":
+                              print("You are Winer ,if you Get a gift '2023441' send this code on telegram group")
+                          else:
+                              print("Game over Start again")
+                    #this else another
+                 else:
+                    print("Game over Start again")
+
+
+           else:
+            print("Game over Start again")
+     
+
+else:
+    print("YOU ARE LOSSER!")
+
+
+
+
+
+
+
+
